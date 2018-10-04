bd2b89c793e1a28309915aa9eb5e2a7383a050c9
glitch452 <894128+glitch452@users.noreply.github.com>
2018-10-04 15:57:08 -0400

    Just Cause

--- app/src/main/java/ca/uottawa/seg2105/project/cqondemand/SignIn.java
+++ app/src/main/java/ca/uottawa/seg2105/project/cqondemand/SignIn.java
@@ -2,6 +2,8 @@ package ca.uottawa.seg2105.project.cqondemand;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignIn extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+    }
+
+    public void onSignInClick(View view){
+        //TextView curText = findViewById(R.id.field_username);
+        //curText.setText("test");
+    }
+
+    public void onCreateAccountClick(View view){
+
     }
 }
