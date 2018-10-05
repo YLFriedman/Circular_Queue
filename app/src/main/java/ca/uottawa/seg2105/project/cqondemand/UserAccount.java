e0d417d835bbfc3fe017aa648d8bb7eb4965f271
glitch452 <894128+glitch452@users.noreply.github.com>
2018-10-04 22:31:48 -0400

    Some updates

--- app/src/main/java/ca/uottawa/seg2105/project/cqondemand/UserAccount.java
+++ app/src/main/java/ca/uottawa/seg2105/project/cqondemand/UserAccount.java
@@ -2,12 +2,66 @@ package ca.uottawa.seg2105.project.cqondemand;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class UserAccount extends AppCompatActivity {
+
+    private enum Views { USER_VIEW, USER_EDIT, CHANGE_PASSWORD }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_account);
     }
+
+    public void onChangePasswordClick(View view){
+        setView(Views.CHANGE_PASSWORD);
+    }
+
+    public void onDeleteAccountClick(View view){
+
+    }
+
+    public void onEditUserClick(View view){
+        setView(Views.USER_EDIT);
+    }
+
+    public void onSaveUserClick(View view){
+
+    }
+
+    public void onSavePasswordClick(View view){
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        LinearLayout layout_user_view = findViewById(R.id.layout_user_view);
+
+        if (layout_user_view.getVisibility() == View.VISIBLE) {
+            super.onBackPressed();
+        } else {
+            setView(Views.USER_VIEW);
+        }
+    }
+
+    private void setView(Views view) {
+
+        LinearLayout layout_user_view = findViewById(R.id.layout_user_view);
+        LinearLayout layout_user_edit = findViewById(R.id.layout_user_edit);
+        LinearLayout layout_change_password = findViewById(R.id.layout_change_password);
+
+        layout_user_view.setVisibility(View.GONE);
+        layout_user_edit.setVisibility(View.GONE);
+        layout_change_password.setVisibility(View.GONE);
+
+        switch (view) {
+            case USER_EDIT: layout_user_edit.setVisibility(View.VISIBLE); return;
+            case CHANGE_PASSWORD: layout_change_password.setVisibility(View.VISIBLE); return;
+            default: layout_user_view.setVisibility(View.VISIBLE);
+        }
+
+    }
+
 }
