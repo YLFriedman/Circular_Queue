b5535f6c58a985a8cd74c4ae864737545014e70d
YLFriedman <37879744+YLFriedman@users.noreply.github.com>
2018-10-06 15:59:45 -0400

    Added a class to represent the User object. Stores relevant data about the current user

---
+++ app/src/main/java/ca/uottawa/seg2105/project/cqondemand/User.java
@@ -0,0 +1,111 @@
+package ca.uottawa.seg2105.project.cqondemand;
+
+public class User {
+
+    /**
+     *The class User allows for the creation of User objects, and stores the pertinent values for each User.
+     *Users can be of type Homeowner, Service Provider or Admin. Getters or Setters for all mutable values
+     *are also provided.
+     *
+     */
+
+    private String fName, lName, uName, email, companyName;
+
+    private final Types type;
+    /**
+     *A simple enum for distinguishing different types of user accounts
+     */
+    private enum Types{
+        ADMIN, HOMEOWNER, SERVICEPROVIDER;
+    }
+    /**
+     *Constructor for User objects. This constructor supports users of type Homeowner and of type Service Provider
+     *
+     *@param fName The first name of the user
+     *@param lName The last name of the user
+     *@param uName The desired Username
+     *@param email The user's email
+     *@param type The type of the account
+     */
+    public User(String fName, String lName, String uName, String email, Types type){
+
+        this.fName = fName;
+        this.lName = lName;
+        this.uName = uName;
+        this.email = email;
+        this.type = type;
+    }
+
+    /**
+     *Simple getter for the user's first name
+     *
+     *@return  The user's first name
+     */
+    public String getFirstName(){
+        return this.fName;
+    }
+
+    /**
+     *Simple getter for the user's last name
+     *
+     *@return The user's last name
+     */
+    public String getLastName(){
+        return this.lName;
+    }
+    /**
+     *Simple getter for the user's username
+     *
+     *@return The user's username
+     */
+    public String getUserName(){
+        return this.uName;
+    }
+
+    /**
+     *Simple getter for the user's email
+     *
+     *@return The user's email
+     */
+    public String getEmail(){
+        return this.email;
+    }
+
+    /**
+     *Simple setter for the user's first name
+     *
+     *@param input The user's new first name
+     */
+    public void setFirstName(String input){
+        fName = input;
+    }
+
+    /**
+     *Simple setter for the user's last name
+     *
+     *@param input The user's new last name
+     */
+    public void setLastName(String input){
+        lName = input;
+    }
+
+    /**
+     *Simple setter for the user's email
+     *
+     *@param input The user's new email
+     */
+    public void setEmail(String input){
+        email = input;
+    }
+
+    /**
+     *Simple setter for the user's username
+     *
+     *@param input The user's new username
+     */
+    public void setUserName(String input){
+        fName = input;
+    }
+
+
+}
