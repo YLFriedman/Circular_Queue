9ce7aa51fd08cc8fabf8dd05326ea2a1a30ad1f8
glitch452 <894128+glitch452@users.noreply.github.com>
2018-10-22 19:42:46 -0400

    stuff

--- app/src/main/java/ca/uottawa/seg2105/project/cqondemand/UserHome.java
+++ app/src/main/java/ca/uottawa/seg2105/project/cqondemand/UserHome.java
@@ -50,30 +50,12 @@ public class UserHome extends AppCompatActivity {
      * Displays Account Details
      */
     public void onAccountDetailsClick(View view) {
-        currentUser = DatabaseUtil.getCurrentUser();
-        if (null == currentUser) {
-            Intent intent = new Intent(this, UserAccount.class);
+        if (null == DatabaseUtil.getCurrentUser()) {
+            Intent intent = new Intent(this, SignIn.class);
             startActivity(intent);
         } else {
-            /*
-            *Hides original layout, makes account details visible
-             */
-            LinearLayout main_view = (LinearLayout) findViewById(R.id.layout_main);
-            main_view.setVisibility(View.GONE);
-            LinearLayout details_view = (LinearLayout) findViewById(R.id.layout_details);
-            details_view.setVisibility(View.VISIBLE);
-
-            TextView txtFirstName = findViewById(R.id.textView_firstName);
-            txtFirstName.setText("First Name: " + currentUser.getFirstName());
-            TextView txtLastName = findViewById(R.id.textView_lastName);
-            txtLastName.setText("Last Name: " + currentUser.getLastName());
-            TextView txtUserName = findViewById(R.id.textView_userName);
-            txtUserName.setText("User Name: " + currentUser.getUserName());
-            TextView txtAccountType = findViewById(R.id.textView_account);
-            txtAccountType.setText("Account type: " + currentUser.getType().toString());
-            TextView txtEmail = findViewById(R.id.textView_email);
-            txtEmail.setText("Current Email: " + currentUser.getEmail());
-
+            Intent intent = new Intent(this, UserAccount.class);
+            startActivity(intent);
         }
     }
 
