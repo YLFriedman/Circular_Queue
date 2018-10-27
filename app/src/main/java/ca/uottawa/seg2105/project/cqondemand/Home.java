a4bb01a9d8902f3168c00ed73e36e1542f0ed43b
glitch452 <894128+glitch452@users.noreply.github.com>
2018-10-27 12:48:45 -0400

    workin on drawer

--- app/src/main/java/ca/uottawa/seg2105/project/cqondemand/Home.java
+++ app/src/main/java/ca/uottawa/seg2105/project/cqondemand/Home.java
@@ -2,16 +2,17 @@ package ca.uottawa.seg2105.project.cqondemand;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -28,6 +29,7 @@ public class Home extends AppCompatActivity {
     private User currentUser;
     private LinearLayout userListContainer;
 
+    private DrawerLayout drawer;
 
     /*
      * Fills in layout for UserHome activity
@@ -37,16 +39,17 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-
-
-        userListContainer = findViewById(R.id.userListContainer);
         currentUser = DatabaseUtil.getCurrentUser();
-        userList = new ArrayList<User>();
 
         if (null == currentUser) {
             onSignOutClick(null);
         } else {
+
+            setupDrawer();
+
+            userListContainer = findViewById(R.id.userListContainer);
             userListContainer.setVisibility(View.GONE);
+            userList = new ArrayList<User>();
             if (currentUser.getType() == User.Types.ADMIN) {
                 recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
                 recyclerView.setHasFixedSize(true);
@@ -79,6 +82,43 @@ public class Home extends AppCompatActivity {
                 });
             }
         }
+    }
+
+    private void setupDrawer() {
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
+        //setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                //getSupportActionBar().setTitle("Navigation!");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                //getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        toggle.setDrawerIndicatorEnabled(true);
+        drawer.addDrawerListener(toggle);
+        if (null != getSupportActionBar()) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
