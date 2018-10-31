348795f56859cf17821956b595db8c4d50cfab94
madison868 <madison.bailey888@gmail.com>
2018-10-31 15:29:16 -0400

    "Initial push for Catagory and Service classes"

---
+++ app/src/main/java/ca/uottawa/seg2105/project/cqondemand/Catagory.java
@@ -0,0 +1,13 @@
+package ca.uottawa.seg2105.project.cqondemand;
+
+import java.util.ArrayList;
+
+public class Catagory {
+
+    public static String CatagoryTypeName;
+    public ArrayList<Service> Services;
+
+    public void Catagory(String Catagorytype){
+                CatagoryTypeName = Catagorytype;
+    }
+}
