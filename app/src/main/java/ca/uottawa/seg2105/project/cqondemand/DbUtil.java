aa45eb4005d93a7c57b79ef26fa208ef005345e4
YLFriedman <37879744+YLFriedman@users.noreply.github.com>
2018-11-07 17:51:36 -0500

    Added Generic DbUtil methods

--- app/src/main/java/ca/uottawa/seg2105/project/cqondemand/DbUtil.java
+++ app/src/main/java/ca/uottawa/seg2105/project/cqondemand/DbUtil.java
@@ -8,6 +8,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.io.UncheckedIOException;
@@ -33,6 +34,18 @@ public class DbUtil {
                 default: return this.name();
             }
         }
+    }
+    @SuppressWarnings("unchecked")
+    public static <T> DbItem<T> objectToDbItem(T object, DataType type) {
+
+        DbItem<T> dbItem = null;
+        switch (type) {
+            case USER: return (DbItem<T>) new DbUser((User) object);
+            case SERVICE: return (DbItem<T>) new DbService((Service) object);
+            case CATEGORY: return (DbItem<T>) new DbCategory((Category) object);
+
+        }
+        return null;
     }
 
     private static Class getClassObj(DataType type) {
@@ -118,6 +131,128 @@ public class DbUtil {
         });
 
 
+    }
+    public static <T> void getItems(final DataType type, String childKey, String childValue, final DbValueEventListener<T> listener){
+        Query query = getRef(type).orderByChild(childKey).equalTo(childValue);
+        getItems(type, query, listener);
+
+    }
+
+    private static <T> void getItems(final DataType type, Query query, final DbValueEventListener<T> listener) {
+        if (null == query) { throw new IllegalArgumentException("The reference cannot be null."); }
+        if (null == listener) { throw new IllegalArgumentException("The listener cannot be null."); }
+
+         query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                long size = dataSnapshot.getChildrenCount();
+                ArrayList<T> returnValue = new ArrayList<T>(size > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) size);
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    try {
+                        DbItem<T> item = (DbItem<T>) snapshot.getValue(getClassObj(type));
+                        T itemValue = item.toItem();
+                        returnValue.add(itemValue);
+                    }
+
+                    catch (IllegalArgumentException e) {
+
+                    }
+                    catch (ClassCastException e){
+
+                    }
+                }
+                listener.onSuccess(returnValue);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                listener.onFailure(DbEventFailureReason.DATABASE_ERROR);
+
+            }
+        });
+
+
+    }
+
+    public static void deleteItem(DataType type, String id, final DbActionEventListener listener){
+        DatabaseReference.CompletionListener complete = new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                if (databaseError == null) {
+                    listener.onSuccess();
+                } else {
+                    listener.onFailure(DbEventFailureReason.DATABASE_ERROR);
+                }
+            }
+        };
+        dbUsers.child(id).removeValue(complete);
+    }
+
+
+
+    @SuppressWarnings("unchecked")
+    public static <T> void createItem(final DataType type, T item, final DbActionEventListener listener){
+        if (null == type) { throw new IllegalArgumentException("The type cannot be null."); }
+        if (null == item) { throw new IllegalArgumentException("The object cannot be null."); }
+        if (null == listener) { throw new IllegalArgumentException("The listener cannot be null."); }
+        final DbItem<T> dbItem = objectToDbItem(item, type);
+        final String key = dbItem.getKey();
+        getItem(type, key, new DbValueEventListener<T>() {
+            @Override
+            public void onSuccess(ArrayList<T> data) {
+                //Failure condition, Item already exists in db
+                listener.onFailure(DbEventFailureReason.ALREADY_EXISTS);
+            }
+
+            @Override
+            public void onFailure(DbEventFailureReason reason) {
+                //Success condition, item can be safely created;
+                if(reason == DbEventFailureReason.DOES_NOT_EXIST) {
+                    DatabaseReference ref = getRef(type);
+                    try {
+                        ref.child(key).setValue(dbItem);
+                        listener.onSuccess();
+                    } catch (DatabaseException e) {
+                        listener.onFailure(DbEventFailureReason.DATABASE_ERROR);
+                    }
+                }
+                else{
+                    listener.onFailure(reason);
+                }
+            }
+        });
+
+    }
+
+    public static <T> void updateItem(final DataType type, T item, final DbActionEventListener listener){
+        if (null == type) { throw new IllegalArgumentException("The type cannot be null."); }
+        if (null == item) { throw new IllegalArgumentException("The object cannot be null."); }
+        if (null == listener) { throw new IllegalArgumentException("The listener cannot be null."); }
+
+        final DbItem<T> dbItem = objectToDbItem(item, type);
+        final String key = dbItem.getKey();
+        getItem(type, key, new DbValueEventListener<T>() {
+            @Override
+            public void onSuccess(ArrayList<T> data){
+                //Success condition, Item exists in database and can be updated
+                DatabaseReference ref = getRef(type);
+                try {
+                    ref.child(key).setValue(dbItem);
+                    listener.onSuccess();
+                }
+
+                catch (DatabaseException e) {
+                    listener.onFailure(DbEventFailureReason.DATABASE_ERROR);
+                }
+            }
+            @Override
+            public void onFailure(DbEventFailureReason reason){
+                //Failure condition, cannot update an item that doesn't currently exist in the db
+                listener.onFailure(reason);
+
+            }
+        });
     }
 
 
@@ -328,7 +463,8 @@ public class DbUtil {
     }
 
     public static abstract class DbItem<T> {
-        public T toItem() { return null; }
+        public abstract T toItem();
+        public abstract String getKey();
     }
     public static class DbUser extends DbItem<User> {
         public String first_name;
@@ -347,6 +483,11 @@ public class DbUtil {
             type = user.getType().toString();
         }
         public User toItem() { return new User(first_name, last_name, username, email, User.parseType(type), password); }
+
+        @Override
+        public String getKey() {
+            return username;
+        }
     }
     public static class DbService extends DbItem<Service> {
         public String name;
@@ -356,14 +497,34 @@ public class DbUtil {
             name = service.getName();
             rate = service.getRate();
         }
+
+        @Override
+        public Service toItem() {
+            return null;
+        }
+
+        @Override
+        public String getKey() {
+            return name;
+        }
         //public User toItem() { return new User(first_name, last_name, username, email, User.parseType(type), password); }
     }
-    public static class DbCategory extends DbItem<Service> {
+    public static class DbCategory extends DbItem<Category> {
         public String name;
 
         public DbCategory() {}
         public DbCategory(Category category) {
             name = category.getName();
+        }
+
+        @Override
+        public Category toItem() {
+            return null;
+        }
+
+        @Override
+        public String getKey() {
+            return name;
         }
         //public User toItem() { return new User(first_name, last_name, username, email, User.parseType(type), password); }
     }
