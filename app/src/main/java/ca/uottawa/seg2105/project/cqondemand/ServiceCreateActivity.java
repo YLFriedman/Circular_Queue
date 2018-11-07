fe021d490139a44d498d8b5fdbaf365e2a0b9061
Tommy <43283387+Txu045@users.noreply.github.com>
2018-11-07 16:58:29 -0500

    quick start

--- app/src/main/java/ca/uottawa/seg2105/project/cqondemand/ServiceCreateActivity.java
+++ app/src/main/java/ca/uottawa/seg2105/project/cqondemand/ServiceCreateActivity.java
@@ -1,7 +1,10 @@
 package ca.uottawa.seg2105.project.cqondemand;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ServiceCreateActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class ServiceCreateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_create);
+        EditText field_service_name = findViewById(R.id.field_service_name);
+        Intent intent = getIntent();
+        field_service_name.setText(intent.getStringExtra("name"));
     }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        setResult(RESULT_CANCELED, intent);
+        finish();
+    }
+
+    public void onCreateCategory(View view){
+        final EditText field_service_name = findViewById(R.id.field_service_name);
+        final String name = field_service_name.getText().toString().trim();
+    }
+
 }
