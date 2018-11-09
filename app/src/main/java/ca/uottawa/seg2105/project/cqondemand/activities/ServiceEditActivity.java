27c1e431b7c9dd9c14018e6e7230bb22b55d0384
Tommy <43283387+Txu045@users.noreply.github.com>
2018-11-09 16:50:25 -0500

    Service create working, quick fix

--- app/src/main/java/ca/uottawa/seg2105/project/cqondemand/activities/ServiceEditActivity.java
+++ app/src/main/java/ca/uottawa/seg2105/project/cqondemand/activities/ServiceEditActivity.java
@@ -4,17 +4,68 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ca.uottawa.seg2105.project.cqondemand.R;
+import ca.uottawa.seg2105.project.cqondemand.domain.Category;
+import ca.uottawa.seg2105.project.cqondemand.utilities.AsyncEventFailureReason;
+import ca.uottawa.seg2105.project.cqondemand.utilities.AsyncValueEventListener;
 import ca.uottawa.seg2105.project.cqondemand.utilities.State;
 
-public class ServiceEditActivity extends AppCompatActivity {
+public class ServiceEditActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    Spinner spinner;
+    String categoryName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_edit);
 
+        EditText field_service_name = findViewById(R.id.field_service_name);
+        Intent intent = getIntent();
+        field_service_name.setText(intent.getStringExtra("name"));
+
+        spinner = findViewById(R.id.spinner_services);
+        spinner.setOnItemSelectedListener(this);
+
+        Category.getCategories(new AsyncValueEventListener<Category>() {
+            @Override
+            public void onSuccess(ArrayList<Category> data) {
+                loadSpinnerData(data);
+            }
+
+            @Override
+            public void onFailure(AsyncEventFailureReason reason) {
+
+            }
+        });
+    }
+
+    private void loadSpinnerData(ArrayList<Category> data){
+
+        List<String> names = new ArrayList<String>();
+        for (Category cat: data){
+            names.add(cat.getName());
+        }
+        names.add(0, "<Select Category>");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, names);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
     }
 
     public void onResume() {
@@ -28,6 +79,18 @@ public class ServiceEditActivity extends AppCompatActivity {
     }
 
     public void onSaveService(View view){
+        
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        categoryName = parent.getItemAtPosition(position).toString();
+
+        Toast.makeText(parent.getContext(), categoryName, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
