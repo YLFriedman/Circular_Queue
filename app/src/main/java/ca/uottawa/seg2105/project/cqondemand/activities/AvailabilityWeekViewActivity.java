231f62e3974597a866e02c336cfb0e277e7f4d64
glitch452 <894128+glitch452@users.noreply.github.com>
2018-11-16 01:54:56 -0500

    some more work done on the week view table functionality

--- app/src/main/java/ca/uottawa/seg2105/project/cqondemand/activities/AvailabilityWeekViewActivity.java
+++ app/src/main/java/ca/uottawa/seg2105/project/cqondemand/activities/AvailabilityWeekViewActivity.java
@@ -1,7 +1,6 @@
 package ca.uottawa.seg2105.project.cqondemand.activities;
 
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.Toast;
 
@@ -9,47 +8,109 @@ import ca.uottawa.seg2105.project.cqondemand.R;
 
 public class AvailabilityWeekViewActivity extends SignedInActivity {
 
-    boolean[][] state;
+    private enum State { DEFAULT, AVAILABLE, BOOKED, UNAVAILABLE }
+    private class Cell {
+        String[] dayNames = new String[] { "sun", "mon", "tue", "wed", "thu", "fri", "sat" };
+        String[] timeNames = new String[] { "00", "01", "02", "03", "04", "05", "06", "07", "08", "09" };
+        public int day;
+        public int time;
+        public State state;
+        public Cell() { state = State.DEFAULT; }
+        public Cell(int day, int time) { this.day = day; this.time = time; this.state = state; state = State.DEFAULT; }
+        public Cell(int day, int time, State state) { this.day = day; this.time = time; this.state = state; }
+        public String getCellName() { return "cell_" + dayNames[day] + "_" + ((time < 10) ? timeNames[time] : time); }
+    }
+
+    View[][] cellViews;
+    Cell[][] cells;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_availability_week_view);
 
-        state = new boolean[7][24];
+        // Create the arrays for the cellViews and cells
+        cellViews = new View[7][24];
+        cells = new Cell[7][24];
 
-    }
-
-    public void onCellClick(View cell) {
-
-        String cellName = cell.getResources().getResourceEntryName(cell.getId());
-        int day = -1;
-        switch (cellName.substring(5,8)) {
-            case "sun": day = 0; break;
-            case "mon": day = 1; break;
-            case "tue": day = 2; break;
-            case "wed": day = 3; break;
-            case "thu": day = 4; break;
-            case "fri": day = 5; break;
-            case "sat": day = 6;
+        // Fill the cellViews and cells arrays
+        for (int day = 0; day < 7; day++) {
+            for (int time = 0; time < 24; time++) {
+                cells[day][time] = new Cell(day, time);
+                cellViews[day][time] = findViewById(getResources().getIdentifier(cells[day][time].getCellName(), "id", getPackageName()));
+                cellViews[day][time].setLongClickable(true);
+                cellViews[day][time].setOnLongClickListener(new CellLongClickListener());
+                cellViews[day][time].setTag(cells[day][time]);
+            }
         }
-        int time = Integer.parseInt(cellName.substring(9));
 
-        toggleCell(cell, day, time);
-
-        //Toast.makeText(getApplicationContext(), "Cell: " + cellName + "\nDay: " + day + "\nTime: " + time, Toast.LENGTH_LONG).show();
-        //Toast.makeText(getApplicationContext(), "getResourceEntryName: " + getResourceEntryName + "\ngetResourceName: " + getResourceName, Toast.LENGTH_LONG).show();
+        //Testing
+        for (int day = 0; day < 7; day++) {
+            for (int time = 0; time < 24; time++) {
+                if ((day % 2 == 0 && time % 2 == 0) || (day % 2 != 0 && time % 2 != 0)) {
+                    cells[day][time].state = State.AVAILABLE;
+                    updateCellView(day, time);
+                }
+            }
+        }
+        updateAllCellViews();
     }
 
-    private void toggleCell(View cell, int day, int time) {
-        if (state[day][time]) {
-            state[day][time] = false;
-            cell.setBackgroundResource(R.drawable.btn_bg_avail_cell_bkrd_default);
+    private void updateAllCellViews() {
+        for (int day = 0; day < 7; day++) {
+            for (int time = 0; time < 24; time++) {
+                updateCellView(day, time);
+            }
+        }
+    }
+
+    private void updateCellView(int day, int time) {
+        switch (cells[day][time].state) {
+            case AVAILABLE: cellViews[day][time].setBackgroundResource(R.drawable.btn_bg_avail_cell_bkrd_available); break;
+            case UNAVAILABLE: cellViews[day][time].setBackgroundResource(R.drawable.btn_bg_avail_cell_bkrd_default); break;
+            case BOOKED: cellViews[day][time].setBackgroundResource(R.drawable.btn_bg_avail_cell_bkrd_booked); break;
+            case DEFAULT: cellViews[day][time].setBackgroundResource(R.drawable.btn_bg_avail_cell_bkrd_default);
+        }
+    }
+
+    private void setCell(int day, int time, State state) {
+        cells[day][time].state = state;
+        updateCellView(day, time);
+    }
+
+    public void onCellClick(View view) {
+        Cell cell;
+        if (view.getTag() instanceof Cell) { cell = (Cell) view.getTag(); }
+        else { return; }
+
+        String toast;
+        toast = "Cell: " + cell.getCellName();
+        //toast += " (" + (cellViews[cell.day][cell.time] == view ? "Y" : "N") + ")";
+
+        if (State.DEFAULT != cell.state) {
+            setCell(cell.day, cell.time, State.DEFAULT);
         } else {
-            state[day][time] = true;
-            cell.setBackgroundResource(R.drawable.btn_bg_avail_cell_bkrd_available);
+            setCell(cell.day, cell.time, State.AVAILABLE);
         }
+
+
+        //Toast.makeText(getApplicationContext(), toast, Toast.LENGTH_LONG).show();
     }
 
+    public void onCellLongPress(View view) {
 
+
+    }
+
+    private class CellLongClickListener implements View.OnLongClickListener {
+
+        public boolean onLongClick(View view) {
+            Cell cell;
+            if (view.getTag() instanceof Cell) { cell = (Cell) view.getTag(); }
+            else { return false; }
+            setCell(cell.day, cell.time, State.BOOKED);
+
+            return true;
+        }
+    }
 }
