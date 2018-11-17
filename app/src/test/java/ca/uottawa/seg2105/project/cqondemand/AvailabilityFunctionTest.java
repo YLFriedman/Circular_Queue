150e4c8c7263d76ed1ca22c1c75a0495c540be40
madison868 <madison.bailey888@gmail.com>
2018-11-16 20:24:32 -0500

    "More changes to Availability test"

--- app/src/test/java/ca/uottawa/seg2105/project/cqondemand/AvailabilityFunctionTest.java
+++ app/src/test/java/ca/uottawa/seg2105/project/cqondemand/AvailabilityFunctionTest.java
@@ -7,9 +7,6 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import ca.uottawa.seg2105.project.cqondemand.domain.Availability;
-import ca.uottawa.seg2105.project.cqondemand.domain.User;
-import ca.uottawa.seg2105.project.cqondemand.utilities.AsyncValueEventListener;
-import ca.uottawa.seg2105.project.cqondemand.utilities.FieldValidation;
 
 import static org.junit.Assert.assertEquals;
 
@@ -32,7 +29,7 @@ public class AvailabilityFunctionTest {
     }
 
     @Test
-    public void validate_ParseDay(){
+    public void validate_ParseDayString(){
         assertEquals("Day validation failed - String_of_letters_Day", Availability.Day.SUNDAY, Availability.parseDay("sunDay"));
         assertEquals("Day validation failed - String_of_letters_Day", Availability.Day.MONDAY, Availability.parseDay("MONDay"));
         assertEquals("Day validation failed - String_of_letters_Day", Availability.Day.TUESDAY, Availability.parseDay("TuesDay"));
@@ -40,5 +37,16 @@ public class AvailabilityFunctionTest {
         assertEquals("Day validation failed - String_of_letters_Day", Availability.Day.THURSDAY, Availability.parseDay("thursday"));
         assertEquals("Day validation failed - String_of_letters_Day", Availability.Day.FRIDAY, Availability.parseDay("Friday"));
         assertEquals("Day validation failed - String_of_letters_Day", Availability.Day.SATURDAY, Availability.parseDay("saTURday"));
+    }
+
+    @Test
+    public void validate_ParseDayInt(){
+        assertEquals("Day validation failed - Int_Day", Availability.Day.SUNDAY, Availability.parseDay(0));
+        assertEquals("Day validation failed - Int_Day", Availability.Day.MONDAY, Availability.parseDay(1));
+        assertEquals("Day validation failed - Int_Day", Availability.Day.TUESDAY, Availability.parseDay(2));
+        assertEquals("Day validation failed - Int_Day", Availability.Day.WEDNESDAY, Availability.parseDay(3));
+        assertEquals("Day validation failed - Int_Day", Availability.Day.THURSDAY, Availability.parseDay(4));
+        assertEquals("Day validation failed - Int_Day", Availability.Day.FRIDAY, Availability.parseDay(5));
+        assertEquals("Day validation failed - Int_Day", Availability.Day.SATURDAY, Availability.parseDay(6));
     }
 }
