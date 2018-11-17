bf14034e0b7ca795569c5ee61d964f6d7e6660b4
madison868 <madison.bailey888@gmail.com>
2018-11-16 20:21:23 -0500

    "Fix"

--- app/src/test/java/ca/uottawa/seg2105/project/cqondemand/AvailabilityFunctionTest.java
+++ app/src/test/java/ca/uottawa/seg2105/project/cqondemand/AvailabilityFunctionTest.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 
 import ca.uottawa.seg2105.project.cqondemand.domain.Availability;
 import ca.uottawa.seg2105.project.cqondemand.domain.User;
+import ca.uottawa.seg2105.project.cqondemand.utilities.AsyncValueEventListener;
+import ca.uottawa.seg2105.project.cqondemand.utilities.FieldValidation;
 
 import static org.junit.Assert.assertEquals;
 
@@ -27,5 +29,16 @@ public class AvailabilityFunctionTest {
         assertEquals("Service getter Failed - day", Availability.Day.SUNDAY, testAvailability.getDay());
         assertEquals("Service getter Failed - startTime", 12, testAvailability.getStartTime());
         assertEquals("Service getter Failed - endTime", 15, testAvailability.getEndTime());
+    }
+
+    @Test
+    public void validate_ParseDay(){
+        assertEquals("Day validation failed - String_of_letters_Day", Availability.Day.SUNDAY, Availability.parseDay("sunDay"));
+        assertEquals("Day validation failed - String_of_letters_Day", Availability.Day.MONDAY, Availability.parseDay("MONDay"));
+        assertEquals("Day validation failed - String_of_letters_Day", Availability.Day.TUESDAY, Availability.parseDay("TuesDay"));
+        assertEquals("Day validation failed - String_of_letters_Day", Availability.Day.WEDNESDAY, Availability.parseDay("WEDNESDAY"));
+        assertEquals("Day validation failed - String_of_letters_Day", Availability.Day.THURSDAY, Availability.parseDay("thursday"));
+        assertEquals("Day validation failed - String_of_letters_Day", Availability.Day.FRIDAY, Availability.parseDay("Friday"));
+        assertEquals("Day validation failed - String_of_letters_Day", Availability.Day.SATURDAY, Availability.parseDay("saTURday"));
     }
 }
