7b63879a15c8fd99fc71c2ce6525d5e3cfab6be9
madison868 <madison.bailey888@gmail.com>
2018-11-19 17:05:37 -0500

    "Db function tests for Address, Availability, service and Category"

---
+++ app/src/test/java/ca/uottawa/seg2105/project/cqondemand/DbAddressFunctionTest.java
@@ -0,0 +1,25 @@
+package ca.uottawa.seg2105.project.cqondemand;
+
+import org.junit.Test;
+
+import ca.uottawa.seg2105.project.cqondemand.database.DbAddress;
+import ca.uottawa.seg2105.project.cqondemand.domain.Address;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class DbAddressFunctionTest {
+    @Test
+    public void validate_Constructor() {
+        DbAddress testDbAddress = new DbAddress(new Address("2a", 12, "Bullvue", "Ottawa", "Ontario", "Canada", "K0A1A0"));
+        Address testAddress = testDbAddress.toDomainObj();
+        assertEquals("Service getter Failed - unit", "2A", testAddress.getUnit());
+        assertEquals("Service getter Failed - streetNumber", 12, testAddress.getStreetNumber());
+        assertEquals("Service getter Failed - street", "Bullvue", testAddress.getStreet());
+        assertEquals("Service getter Failed - city", "Ottawa", testAddress.getCity());
+        assertEquals("Service getter Failed - city", "Ontario", testAddress.getProvince());
+        assertEquals("Service getter Failed - country", "Canada", testAddress.getCountry());
+        assertEquals("Service getter Failed - postalCode", "K0A 1A0", testAddress.getPostalCode());
+    }
+
+}
