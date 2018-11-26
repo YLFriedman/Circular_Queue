3d86a369df199fbec744e8a9f0488687d12a84e8
glitch452 <894128+glitch452@users.noreply.github.com>
2018-11-26 12:12:52 -0500

    Starting to build out timeslot selection for homeowner

--- app/src/main/java/ca/uottawa/seg2105/project/cqondemand/activities/WeekViewActivity.java
+++ app/src/main/java/ca/uottawa/seg2105/project/cqondemand/activities/WeekViewActivity.java
@@ -1,6 +1,7 @@
 package ca.uottawa.seg2105.project.cqondemand.activities;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -15,8 +16,8 @@ import java.util.ArrayList;
 import ca.uottawa.seg2105.project.cqondemand.R;
 import ca.uottawa.seg2105.project.cqondemand.database.DbAvailability;
 import ca.uottawa.seg2105.project.cqondemand.domain.Availability;
+import ca.uottawa.seg2105.project.cqondemand.domain.Service;
 import ca.uottawa.seg2105.project.cqondemand.domain.ServiceProvider;
-import ca.uottawa.seg2105.project.cqondemand.domain.User;
 import ca.uottawa.seg2105.project.cqondemand.utilities.AsyncActionEventListener;
 import ca.uottawa.seg2105.project.cqondemand.utilities.AsyncEventFailureReason;
 import ca.uottawa.seg2105.project.cqondemand.utilities.AsyncValueEventListener;
@@ -24,10 +25,10 @@ import ca.uottawa.seg2105.project.cqondemand.utilities.State;
 
 public class WeekViewActivity extends SignedInActivity {
 
-    private enum CellState { UNAVAILABLE, AVAILABLE, BOOKED, REQUESTED }
-    private class Cell {
-        String[] dayNames = new String[] { "sun", "mon", "tue", "wed", "thu", "fri", "sat" };
-        String[] timeNames = new String[] { "00", "01", "02", "03", "04", "05", "06", "07", "08", "09" };
+    protected enum CellState { UNAVAILABLE, AVAILABLE, BOOKED, REQUESTED }
+    protected class Cell {
+        public String[] dayNames = new String[] { "sun", "mon", "tue", "wed", "thu", "fri", "sat" };
+        public String[] timeNames = new String[] { "00", "01", "02", "03", "04", "05", "06", "07", "08", "09" };
         public int day;
         public int time;
         public CellState cellState;
@@ -37,15 +38,41 @@ public class WeekViewActivity extends SignedInActivity {
         public String getCellName() { return "cell_" + dayNames[day] + "_" + ((time < 10) ? timeNames[time] : time); }
     }
 
+    protected enum Mode { SELECT_TIMESLOT, AVAILABILITY }
+    protected Mode mode;
     protected boolean itemClickEnabled = true;
     protected View[][] cellViews;
     protected Cell[][] cells;
-    protected User currentUser;
+    protected ServiceProvider currentProvider;
+    protected Service currentService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_week_view_availability);
+
+        Intent intent = getIntent();
+        try {
+            currentProvider = (ServiceProvider) intent.getSerializableExtra("provider");
+            currentService = (Service) intent.getSerializableExtra("service");
+        } catch (ClassCastException e) {
+            // TODO: Toast message for invalid type
+            finish();
+            return;
+        }
+
+        if ((null == currentProvider) == (null == currentService) && null != currentProvider) {
+            mode = Mode.SELECT_TIMESLOT;
+            setContentView(R.layout.activity_week_view_availability);
+        } else if (State.getInstance().getSignedInUser() instanceof ServiceProvider) {
+            mode = Mode.AVAILABILITY;
+            setContentView(R.layout.activity_week_view_calendar);
+            currentProvider = (ServiceProvider) State.getInstance().getSignedInUser();
+            onRestoreSavedClick();
+        } else {
+            Toast.makeText(getApplicationContext(), R.string.service_provider_required, Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
 
         // Create the arrays for the cellViews and cells
         cellViews = new View[7][24];
@@ -62,20 +89,14 @@ public class WeekViewActivity extends SignedInActivity {
             }
         }
 
-        currentUser = State.getInstance().getSignedInUser();
-
-        if (currentUser instanceof ServiceProvider) {
-            onRestoreSavedClick();
-        } else {
-            Toast.makeText(getApplicationContext(), R.string.service_provider_required, Toast.LENGTH_LONG).show();
-            finish();
-        }
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.availabilities_options, menu);
+        getMenuInflater().inflate(R.menu.week_view_options, menu);
+        if (Mode.AVAILABILITY != mode) {
+            menu.setGroupVisible(R.id.grp_avail_controls, false);
+        }
         return true;
     }
 
@@ -98,28 +119,38 @@ public class WeekViewActivity extends SignedInActivity {
     }
 
     public void onSaveClick(View view) {
-        if (!itemClickEnabled) { return; }
-        itemClickEnabled = false;
-        DbAvailability.setAvailabilities((ServiceProvider) currentUser, Availability.toList(getAvailabilities()), new AsyncActionEventListener() {
-            @Override
-            public void onSuccess() {
-                Toast.makeText(getApplicationContext(), getString(R.string.availability_saved_successfully), Toast.LENGTH_LONG).show();
-                itemClickEnabled = true;
-            }
-            @Override
-            public void onFailure(@NonNull AsyncEventFailureReason reason) {
-                Toast.makeText(getApplicationContext(), getString(R.string.availability_save_failed), Toast.LENGTH_LONG).show();
-                itemClickEnabled = true;
-            }
-        });
+        if (Mode.AVAILABILITY == mode) {
+            if (!itemClickEnabled) { return; }
+            itemClickEnabled = false;
+            DbAvailability.setAvailabilities(currentProvider, Availability.toList(getAvailabilities()), new AsyncActionEventListener() {
+                @Override
+                public void onSuccess() {
+                    Toast.makeText(getApplicationContext(), getString(R.string.availability_saved_successfully), Toast.LENGTH_LONG).show();
+                    itemClickEnabled = true;
+                }
+                @Override
+                public void onFailure(@NonNull AsyncEventFailureReason reason) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.availability_save_failed), Toast.LENGTH_LONG).show();
+                    itemClickEnabled = true;
+                }
+            });
+        }
     }
 
     private void onHelpClick() {
-
         if (!itemClickEnabled) { return; }
         itemClickEnabled = false;
+        View helpView;
+        if (Mode.AVAILABILITY == mode) {
+            helpView = getLayoutInflater().inflate(R.layout.help_availabilities, null);
+        } else if (Mode.SELECT_TIMESLOT == mode) {
+            helpView = getLayoutInflater().inflate(R.layout.help_select_timeslot, null);
+        } else {
+            itemClickEnabled = true;
+            return;
+        }
         AlertDialog dialog = new AlertDialog.Builder(this)
-                .setView(getLayoutInflater().inflate(R.layout.help_availabilities, null))
+                .setView(helpView)
                 .setPositiveButton(R.string.ok, null)
                 .setOnDismissListener(new DialogInterface.OnDismissListener() {
                     @Override
@@ -127,7 +158,6 @@ public class WeekViewActivity extends SignedInActivity {
                 })
                 .show();
         dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.dialog_red));
-
     }
 
     private void onClearClick() {
@@ -137,7 +167,7 @@ public class WeekViewActivity extends SignedInActivity {
     private void onRestoreSavedClick() {
         if (!itemClickEnabled) { return; }
         itemClickEnabled = false;
-        DbAvailability.getAvailabilities((ServiceProvider) currentUser, new AsyncValueEventListener<Availability>() {
+        DbAvailability.getAvailabilities(currentProvider, new AsyncValueEventListener<Availability>() {
             @Override
             public void onSuccess(@NonNull ArrayList<Availability> data) {
                 setAvailabilities(Availability.toArrays(data));
@@ -185,10 +215,12 @@ public class WeekViewActivity extends SignedInActivity {
     }
 
     public void onCellClick(View view) {
-        Cell cell;
-        if (view.getTag() instanceof Cell) { cell = (Cell) view.getTag(); }
-        else { return; }
-        toggleAvailability(cell.day, cell.time);
+        if (Mode.AVAILABILITY == mode) {
+            Cell cell;
+            if (view.getTag() instanceof Cell) { cell = (Cell) view.getTag(); }
+            else { return; }
+            toggleAvailability(cell.day, cell.time);
+        }
     }
 
     public void toggleAvailability(int day, int time) {
@@ -202,22 +234,24 @@ public class WeekViewActivity extends SignedInActivity {
 
     private class CellLongClickListener implements View.OnLongClickListener {
         public boolean onLongClick(View view) {
-            Cell cell;
-            if (view.getTag() instanceof Cell) { cell = (Cell) view.getTag(); }
-            else { return false; }
+            if (Mode.AVAILABILITY == mode) {
+                Cell cell;
+                if (view.getTag() instanceof Cell) { cell = (Cell) view.getTag(); }
+                else { return false; }
 
-            int time = cell.time;
-            int day = cell.day;
+                int time = cell.time;
+                int day = cell.day;
 
-            if (CellState.UNAVAILABLE == cell.cellState) {
-                do { setCell(day, time--, CellState.AVAILABLE); }
-                while (time >= 0 && CellState.UNAVAILABLE == cells[day][time].cellState);
-            } else if (CellState.AVAILABLE == cell.cellState) {
-                do { setCell(day, time--, CellState.UNAVAILABLE); }
-                while (time >= 0 && CellState.AVAILABLE == cells[day][time].cellState);
-                time = cell.time + 1;
-                while (time <= 23 && CellState.AVAILABLE == cells[day][time].cellState) {
-                    setCell(day, time++, CellState.UNAVAILABLE);
+                if (CellState.UNAVAILABLE == cell.cellState) {
+                    do { setCell(day, time--, CellState.AVAILABLE); }
+                    while (time >= 0 && CellState.UNAVAILABLE == cells[day][time].cellState);
+                } else if (CellState.AVAILABLE == cell.cellState) {
+                    do { setCell(day, time--, CellState.UNAVAILABLE); }
+                    while (time >= 0 && CellState.AVAILABLE == cells[day][time].cellState);
+                    time = cell.time + 1;
+                    while (time <= 23 && CellState.AVAILABLE == cells[day][time].cellState) {
+                        setCell(day, time++, CellState.UNAVAILABLE);
+                    }
                 }
             }
 
