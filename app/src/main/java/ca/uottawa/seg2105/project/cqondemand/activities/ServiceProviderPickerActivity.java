bfcd8e5884b4d75b19ce4919dfd18c8d3d5e6d96
glitch452 <894128+glitch452@users.noreply.github.com>
2018-11-28 23:41:27 -0500

    SP Filter GUI working, now to make it actually filter the items!

--- app/src/main/java/ca/uottawa/seg2105/project/cqondemand/activities/ServiceProviderPickerActivity.java
+++ app/src/main/java/ca/uottawa/seg2105/project/cqondemand/activities/ServiceProviderPickerActivity.java
@@ -10,14 +10,23 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.Timepoint;
+
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 import ca.uottawa.seg2105.project.cqondemand.R;
 import ca.uottawa.seg2105.project.cqondemand.adapters.ServiceProviderPickerListAdapter;
@@ -40,18 +49,26 @@ public class ServiceProviderPickerActivity extends SignedInActivity {
     private Service currentService;
     private TextView txt_empty_list_message;
     private int filterMinRating;
-    private int filterDate = -1;
+    private int filterDay = -1;
     private int filterStartTime = -1;
     private int filterEndTime = -1;
+    private int selectedStartTime = -1;
+    private int selectedEndTime = -1;
+    private int tempStartTime;
+    protected Calendar cal = Calendar.getInstance(Locale.CANADA);
+    private SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("h:mm a", Locale.CANADA);
     private ArrayList<ServiceProvider> allProviders;
     private AlertDialog filterDialog;
     private View filterView;
     private TextView txt_clear_availability;
     private TextView txt_clear_rating;
+    private TextView txt_set_time;
     private RatingBar stars_filter_rating;
     private Spinner spinner_day_filter;
     private SpinnerAdapter<Availability.Day> dayFilterAdapter;
     private TextView txt_time_filter;
+    private LinearLayout grp_time;
+    private Timepoint[] selectableTimes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,6 +92,13 @@ public class ServiceProviderPickerActivity extends SignedInActivity {
             finish();
             return;
         }
+
+        selectableTimes = new Timepoint[24];
+        for (int i = 0; i < 24; i++) {
+
+        }
+        //Timepoint[] = new Timepoint()
+        //(new Timepoint(4)).
 
         recycler_list.setHasFixedSize(true);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
@@ -166,38 +190,6 @@ public class ServiceProviderPickerActivity extends SignedInActivity {
         itemClickEnabled = true;
     }*/
 
-    private void setupFilterView() {
-        filterView = getLayoutInflater().inflate(R.layout.dialog_provider_filters, null);
-        txt_clear_availability = filterView.findViewById(R.id.txt_clear_availability);
-        txt_clear_rating = filterView.findViewById(R.id.txt_clear_rating);
-        stars_filter_rating = filterView.findViewById(R.id.stars_filter_rating);
-        spinner_day_filter = filterView.findViewById(R.id.spinner_day_filter);
-        txt_time_filter = filterView.findViewById(R.id.txt_time_filter);
-
-        txt_clear_rating.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) { stars_filter_rating.setRating(0); }
-        });
-        ArrayList<Availability.Day> dayList = new ArrayList<Availability.Day>(Arrays.asList(Availability.Day.values()));
-        dayList.add(0, null);
-        dayFilterAdapter = new SpinnerAdapter<Availability.Day>(getApplicationContext(), R.layout.spinner_item_title, getString(R.string.any_day), dayList);
-        spinner_day_filter.setAdapter(dayFilterAdapter);
-
-        txt_clear_availability.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) { spinner_day_filter.setSelection(0); }
-        });
-    }
-
-    private void setFilterValues() {
-        stars_filter_rating.setRating(filterMinRating);
-    }
-
-    private void getFilterValues() {
-        int newMinRating = (int) stars_filter_rating.getRating();
-        filterMinRating = newMinRating;
-    }
-
     protected void showFilterSettings() {
         if (!itemClickEnabled) { return; }
         itemClickEnabled = false;
@@ -210,8 +202,7 @@ public class ServiceProviderPickerActivity extends SignedInActivity {
                     .setIcon(R.drawable.ic_filter_list_med_24)
                     .setPositiveButton(R.string.apply, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int whichButton) {
-                            getFilterValues();
-                            setListAdapter();
+                            if (getFilterValues()) { setListAdapter(); }
                         }
                     })
                     .setOnDismissListener(new DialogInterface.OnDismissListener() {
@@ -224,6 +215,128 @@ public class ServiceProviderPickerActivity extends SignedInActivity {
         } else {
             filterDialog.show();
         }
+    }
+
+    private void setFilterValues() {
+        stars_filter_rating.setRating(filterMinRating);
+        spinner_day_filter.setSelection(filterDay + 1);
+        if (filterDay < 0) {
+            grp_time.setVisibility(View.GONE);
+            selectedStartTime = -1;
+            selectedEndTime = -1;
+        } else {
+            selectedStartTime = filterStartTime;
+            selectedEndTime = filterEndTime;
+            txt_time_filter.setText(getTimeString(selectedStartTime, selectedEndTime));
+        }
+    }
+
+    private boolean getFilterValues() {
+        int newMinRating = (int) stars_filter_rating.getRating();
+        filterMinRating = newMinRating;
+        filterDay = spinner_day_filter.getSelectedItemPosition() - 1;
+        filterStartTime = selectedStartTime;
+        filterEndTime = selectedEndTime;
+        return true;
+    }
+
+    private void setupFilterView() {
+        filterView = getLayoutInflater().inflate(R.layout.dialog_provider_filters, null);
+        txt_clear_availability = filterView.findViewById(R.id.txt_clear_availability);
+        txt_clear_rating = filterView.findViewById(R.id.txt_clear_rating);
+        txt_set_time = filterView.findViewById(R.id.txt_set_time);
+        stars_filter_rating = filterView.findViewById(R.id.stars_filter_rating);
+        spinner_day_filter = filterView.findViewById(R.id.spinner_day_filter);
+        txt_time_filter = filterView.findViewById(R.id.txt_time_filter);
+        grp_time = filterView.findViewById(R.id.grp_time);
+        grp_time.setVisibility(View.GONE);
+
+        txt_clear_rating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { stars_filter_rating.setRating(0); }
+        });
+        ArrayList<Availability.Day> dayList = new ArrayList<Availability.Day>(Arrays.asList(Availability.Day.values()));
+        dayList.add(0, null);
+        dayFilterAdapter = new SpinnerAdapter<Availability.Day>(getApplicationContext(), R.layout.spinner_item_title, getString(R.string.any_day), dayList);
+        spinner_day_filter.setAdapter(dayFilterAdapter);
+        spinner_day_filter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (0 == position) { return; }
+                grp_time.setVisibility(View.VISIBLE);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) { }
+        });
+
+        txt_clear_availability.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spinner_day_filter.setSelection(0);
+                selectedStartTime = -1;
+                selectedEndTime = -1;
+                txt_time_filter.setText(getString(R.string.any_time));
+                grp_time.setVisibility(View.GONE);
+            }
+        });
+
+        txt_set_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int startWith = selectedStartTime < 0 ? 0 : selectedStartTime;
+                TimePickerDialog tpd = TimePickerDialog.newInstance(getStartTimeListener(), startWith,0,false);
+                tpd.setAccentColor(getResources().getColor(R.color.colorPrimary));
+                tpd.setOkColor(getResources().getColor(R.color.dialog_red));
+                tpd.setCancelColor(getResources().getColor(R.color.text_secondary_dark));
+                tpd.enableMinutes(false);
+                tpd.setTitle(getString(R.string.start_time));
+                tpd.show(getSupportFragmentManager(), "start_time");
+            }
+        });
+
+    }
+
+    private TimePickerDialog.OnTimeSetListener getStartTimeListener() {
+        return new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePickerDialog view, int hourOfDay, int minute, int second) {
+                tempStartTime = hourOfDay;
+                TimePickerDialog tpd = TimePickerDialog.newInstance(getEndTimeListener(),hourOfDay + 1,0,false);
+                tpd.setAccentColor(getResources().getColor(R.color.colorPrimary));
+                tpd.setOkColor(getResources().getColor(R.color.dialog_red));
+                tpd.setCancelColor(getResources().getColor(R.color.text_secondary_dark));
+                tpd.setMinTime(hourOfDay + 1, 0, 0);
+                tpd.enableMinutes(false);
+                tpd.setTitle(getString(R.string.end_time));
+                tpd.show(getSupportFragmentManager(), "end_time");
+            }
+        };
+    }
+
+    private TimePickerDialog.OnTimeSetListener getEndTimeListener() {
+        return new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePickerDialog view, int hourOfDay, int minute, int second) {
+                Toast.makeText(getApplicationContext(), "ST: " + tempStartTime + ", ET: " + hourOfDay, Toast.LENGTH_LONG).show();
+                selectedStartTime = tempStartTime;
+                selectedEndTime = hourOfDay;
+                txt_time_filter.setText(getTimeString(selectedStartTime, selectedEndTime));
+            }
+        };
+    }
+
+    private String getTimeString(int start, int end) {
+        String time = "";
+        if (start >=0 && end >= 0) {
+            cal.setTime(new Date());
+            cal.set(0, 0, 0, start, 0, 0);
+            time += TIME_FORMAT.format(cal.getTime());
+            cal.set(0, 0, 0, end, 0, 0);
+            time += " to " + TIME_FORMAT.format(cal.getTime());
+        } else {
+            time = getString(R.string.any_time);
+        }
+        return time;
     }
 
 }
