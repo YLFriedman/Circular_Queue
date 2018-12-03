46a5e956ec1575878461e7cc0f87df538169a0f8
Tommy Xu <txu045@uottawa.ca>
2018-12-02 22:36:08 -0500

    comments

--- app/src/main/java/ca/uottawa/seg2105/project/cqondemand/adapters/ServiceListAdapter.java
+++ app/src/main/java/ca/uottawa/seg2105/project/cqondemand/adapters/ServiceListAdapter.java
@@ -17,26 +17,63 @@ import ca.uottawa.seg2105.project.cqondemand.domain.Service;
 
 /**
  * An adapter class for the RecyclerView that contains a list of services
+ *
+ * Course: SEG 2105 B
+ * Final Project
+ * Group: CircularQueue
+ *
+ * @author CircularQueue
  */
 
 public class ServiceListAdapter extends RecyclerView.Adapter<ServiceListAdapter.ViewHolder> {
 
+    /**
+     * Stores a custom icon image to be placed
+     */
     private int icon = R.drawable.ic_chevron_right_med_30;
+    /**
+     * The application context that the adapter is being used in
+     */
     private Context context;
+    /**
+     * Stores the list of Service objects data
+     */
     private ArrayList<Service> data;
+    /**
+     * Stores callback when a view is clicked
+     */
     private View.OnClickListener clickListener;
 
+    /**
+     * Constructor for the ServiceListAdapter, does not have an OnClickListener
+     * @param context   Collection of relevant application data for the Service list adapter
+     * @param data      ArrayList of category data to be adapted
+     * @param iconResID ID of the icon image to be used
+     */
     public ServiceListAdapter(Context context, ArrayList<Service> data, int iconResID) {
         this.context = context;
         this.data = data;
         this.icon = iconResID;
     }
 
+    /**
+     * Constructor for the ServiceListAdapter
+     * @param context   Collection of relevant application data for the Service list adapter
+     * @param data      ArrayList of category data to be adapted
+     * @param iconResID ID of the icon image to be used
+     * @param clickListener Callback invoked when a view is clicked
+     */
     public ServiceListAdapter(Context context, ArrayList<Service> data, int iconResID, View.OnClickListener clickListener) {
         this(context, data, iconResID);
         this.clickListener = clickListener;
     }
 
+    /**
+     * Creates a new viewHolder when there are no existing viewHolders avaliable that the recycler can reuse
+     * @param parent    The ViewGroup into which the new View will be added after it is bound to an adapter position
+     * @param viewType  For different types of cells in list - not needed here
+     * @return  new ViewHolder
+     */
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -45,6 +82,11 @@ public class ServiceListAdapter extends RecyclerView.Adapter<ServiceListAdapter.
         return new ViewHolder(view);
     }
 
+    /**
+     * For each recycler holder, populate recycler with data and update the viewholder
+     * @param viewHolder    viewHolder to be updated
+     * @param i Position index from where the data is to be received
+     */
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
         Service item = data.get(i);
@@ -61,17 +103,28 @@ public class ServiceListAdapter extends RecyclerView.Adapter<ServiceListAdapter.
         viewHolder.img_nav.setImageResource(icon);
     }
 
+    /**
+     * Get the total data size associated with the service list
+     * @return size of data
+     */
     @Override
     public int getItemCount() {
         return data.size();
     }
 
+    /**
+     * ViewHolder class to be used with the recycler view
+     */
     class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView txt_title;
         TextView txt_subtitle;
         ImageView img_nav;
 
+        /**
+         * ViewHolder constructor
+         * @param itemView  itemView data to be used
+         */
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             txt_title = itemView.findViewById(R.id.txt_title);
